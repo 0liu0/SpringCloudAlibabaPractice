8046953db6c8819299daf9bf123112276cffdb4a
liuche <lzc706716852@gmail.com>
2023-03-26 23:18:05 +0800

    1-5 修改负载均衡策略

--- nacos-consumer-9100/src/main/java/com/liuche/service/FeignService9100.java
+++ nacos-consumer-9100/src/main/java/com/liuche/service/FeignService9100.java
@@ -1,9 +1,12 @@
 package com.liuche.service;
 
+import com.liuche.config.OpenFeignConfig;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "nacos-service")
+@LoadBalancerClient(name = "nacos-service",configuration = OpenFeignConfig.class)
 public interface FeignService9100 {
     @GetMapping("/service-test/getport")
     String getPort();
