da1dfc7f62e29aae2b992a81a1b9d01371bbf345
liuche <lzc706716852@gmail.com>
2023-03-29 20:16:31 +0800

    1-13 对gateway做了一个快速入门

---
+++ cloud-gateway-9200/src/main/java/com/liuche/CloudGateWay.java
@@ -0,0 +1,11 @@
+package com.liuche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGateWay {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGateWay.class,args);
+    }
+}
